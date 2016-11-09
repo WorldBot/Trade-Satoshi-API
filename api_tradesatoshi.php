81e96dd13467bf28ec8f2ac9121dc4b62845cb33
WorldBot <yannic@laposte.net>
2016-11-09 18:43:52 -0500

    Creation

---
+++ api_tradesatoshi.php
@@ -0,0 +1,144 @@
+<?php
+	function api_query($ENDPOINT, array $REQ = array())
+	{
+		$API_PUBLIC_KEY = ''; // Your Public Api Key
+		$API_SECRET_KEY = ''; // Your Private Api Key
+
+		$PUBLIC_API 	= array('GetCurrencies','GetTicker','GetMarketHistory','GetMarketSummary','GetMarketSummaries','GetOrderBook');
+		$PRIVATE_API 	= array('GetBalance','GetBalances','GetOrder','GetOrders','SubmitOrder','CancelOrder','GetTradeHistory','GenerateAddress','SubmitWithdraw','GetDeposits','GetWithdrawals');
+
+		// Init curl
+		static $ch = null; $ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+		curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/4.0 (compatible; TradeSatoshi API PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
+
+		// remove those 2 line to secure after test.
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+		// PUBLIC or PRIVATE API
+		if (in_array($ENDPOINT,$PUBLIC_API))
+		{
+			$URL = "https://www.tradesatoshi.com/api/public/".$ENDPOINT;
+			if ($REQ) $URL .= '?'.http_build_query($REQ,'','&');
+			curl_setopt($ch, CURLOPT_URL, $URL );
+		}
+		elseif (in_array($ENDPOINT,$PRIVATE_API))
+		{
+			$URL = "https://www.tradesatoshi.com/api/private/".$ENDPOINT;
+			$mt = explode(' ', microtime()); $NONCE = $mt[1].substr($mt[0], 2, 6);
+			$REQ = json_encode($REQ);
+			$SIGNATURE = $API_PUBLIC_KEY.'POST'.strtolower(urlencode($URL)).$NONCE.base64_encode($REQ);
+			$HMAC_SIGN = base64_encode(hash_hmac('sha512',$SIGNATURE,base64_decode($API_SECRET_KEY),true));
+			$HEADER = 'Basic '.$API_PUBLIC_KEY.':'.$HMAC_SIGN.':'.$NONCE;
+			$HEADERS = array("Content-Type: application/json; charset=utf-8", "Authorization: $HEADER");
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $HEADERS);
+			curl_setopt($ch, CURLOPT_URL, $URL );
+			curl_setopt($ch, CURLOPT_POSTFIELDS,$REQ);
+		}
+
+		// run the query
+		$res = curl_exec($ch);
+		if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
+		return json_decode($res);
+	}
+
+
+	/* ---------- Public Functions ---------- */
+	function GetCurrencies()
+		{return api_query("GetCurrencies");}
+
+	function GetTicker($market=false)
+		{if($market===false)return (object)array("success"=>0,"message"=>'market name e.g. "LTC_BTC" (required)'); else return api_query("GetTicker",array('market'=>$market));}
+
+	function GetMarketHistory($market=false,$count=20)
+		{if($market===false)return (object)array("success"=>0,"message"=>'market name e.g. "LTC_BTC" (required)'); else return api_query("GetMarketHistory",array('market'=>$market,'count'=>$count));}
+
+	function GetMarketSummary($market=false)
+		{if($market===false)return (object)array("success"=>0,"message"=>'market name e.g. "LTC_BTC" (required)'); else return api_query("GetMarketSummary",array('Market'=>$market));}
+
+	function GetMarketSummaries()
+		{return api_query("GetMarketSummaries");}
+
+	function GetOrderBook($market=false,$type='both',$depth=20)
+		{if($market===false)return (object)array("success"=>0,"message"=>'market name e.g. "LTC_BTC" (required)'); else return api_query("GetOrderBook",array('Market'=>$market,'Type'=>$type,'Depth'=>$depth));}
+
+
+	/* ---------- Privates Functions ---------- */
+
+	/*	GetBalance
+		Currency: The currency of the balance to return e.g. 'BTC' (required)
+	*/
+	function GetBalance($currency=false)
+		{if($currency===false)return (object)array("success"=>0,"message"=>'currency name e.g. "BTC" (required)'); else return api_query("GetBalance",array('Currency'=>$currency));}
+
+	// No Params
+	function GetBalances()
+		{return api_query("GetBalances");}
+
+	/*	GetOrder
+		OrderId: The order to return (required)
+	*/
+	function GetOrder($orderid=false)
+		{if($orderid===false)return (object)array("success"=>0,"message"=>'OrderId required!'); else return api_query("GetOrder",array('OrderId'=>$orderid));}
+
+	/*	GetOrders
+		Market: The market name e.g. 'LTC_BTC' (optional, default: 'all')
+		Count: The maximum count of records to return (optional, deefault: 20)
+	*/
+	function GetOrders($market="all",$count=20)
+		{return api_query("GetOrders",array('Market'=>$market,'Count'=>$count));}
+
+	/*	SubmitOrder
+		Market: The market name e.g. 'LTC_BTC' (required)
+		Type: The order type name e.g. 'Buy', 'Sell' (required)
+		Amount: The amount to buy/sell (required)
+		Price: The price to buy/sell for (required)
+	*/
+	function SubmitOrder($market=false,$type=false,$amount=false,$price=false)
+		{if($market===false||$type===false||$amount===false||$price===false)return (object)array("success"=>0,"message"=>'Market,Type,Amount,Price are required!'); else return api_query("SubmitOrder",array('Market'=>$market,'Type'=>$type,'Amount'=>$amount,'Price'=>$price));}
+
+	/*	CancelOrder
+		Type: The cancel type, options: 'Single','Market','MarketBuys','MarketSells','AllBuys','AllSells','All'(required)
+		OrderId: The order to cancel(required if cancel type 'Single')
+		Market: The order to cancel(required if cancel type 'Market','MarketBuys','MarketSells')
+	*/
+	function CancelOrder($type=false,$orderid=false,$market=false)
+		{if($type===false)return (object)array("success"=>0,"message"=>'Type and/or OrderId and/or Market are required!'); else return api_query("CancelOrder",array('Type'=>$type,'OrderId'=>$orderid,'Market'=>$market));}
+
+	/*	GetTradeHistory
+		Market: The market name e.g. 'LTC_BTC' (optional, default: 'all')
+		Count: The maximum count of records to return (optional, default: 20)
+	*/
+	function GetTradeHistory($market='all',$count=20)
+		{return api_query("GetTradeHistory",array('Market'=>$market,'Count'=>$count));}
+
+	/*	GenerateAddress
+		Currency: The currency to generate address for e.g. 'BTC' (required)
+	*/
+	function GenerateAddress($currency=false)
+		{if($currency===false)return (object)array("success"=>0,"message"=>'The currency to generate address for e.g. "BTC" (required)'); else return api_query("GenerateAddress",array('Currency'=>$currency));}
+
+	/*	SubmitWithdraw
+		Currency: The currency name e.g. 'BTC' (required)
+		Address: The receiving address (required)
+		Amount: The amount to withdraw (required)
+	*/
+	function SubmitWithdraw($currency=false,$address=false,$amount=false)
+		{if($currency===false||$address===false||$amount===false)return (object)array("success"=>0,"message"=>'Currency,Address,Amount are required'); else return api_query('SubmitWithdraw',array('Currency'=>$currency,'Address'=>$address,'Amount'=>$amount));}
+
+	/*	GetDeposits
+		Currency: The currency name e.g. 'BTC' (optional, default: 'all')
+		Count: The maximum count of records to return (optional, default: 20)
+	*/
+	function GetDeposits($currency='all',$count=20)
+		{return api_query('GetDeposits',array('Currency'=>$currency,'Count'=>$count));}
+
+	/*	GetWithdrawals
+		Currency: The currency name e.g. 'BTC' (optional, default: 'all')
+		Count: The maximum count of records to return (optional, default: 20)
+	*/
+	function GetWithdrawals($currency='all',$count=20)
+		{return api_query('GetWithdrawals',array('Currency'=>$currency,'Count'=>$count));}
+
+?>
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+	// feed $API_PUBLIC_KEY	& $API_SECRET_KEY in api_tradesatoshi.php file
+	@include('api_tradesatoshi.php');
+
+	/* ----- Public Functions ----- */
+
+	 $R = GetCurrencies();
+
+	// $R = GetTicker('NKA_BTC'); // market
+
+	// $R = GetMarketHistory('NKA_BTC',20); // market,count
+
+	// $R = GetMarketSummary('NKA_BTC'); // market
+
+	// $R = GetMarketSummaries();
+
+	// $R =  GetOrderBook('LTC_BTC','Buy',20); // market,type,count
+
+
+	/* ----- Private Functions ----- */
+
+	// $R = GetBalance("BTC"); // currency
+
+	// $R = GetBalances();
+
+	// $R = GetOrder(1001); //OrderId
+
+	// $R = GetOrders('LTC_BTC',20); //Market,Count
+
+	// $R = SubmitOrder('NKA_BTC','Buy',10000,0.00000002); //Market,Type,Amount,Price
+
+	// $R = CancelOrder('Single',1001,''); //Type,OrderId,Market
+
+	// $R = GetTradeHistory('NKA_BTC',20); //Market,Count
+
+	// $R = GenerateAddress('PPC'); //Currency
+
+	// $R = SubmitWithdraw('DOGE','DLciqeWyaG8wQ7sK96hcR33Fguovk8betK',13); //Currency,Address,Amount
+
+	// $R = GetDeposits('DOGE'); //Currency
+
+	// $R = GetWithdrawals('DOGE',20); //Currency,Count
+
+
+	/* ----- RESULTS ----- */
+	if ($R->message)
+		echo '<hr />'.($R->message).'<hr />';
+
+	var_dump($R);
+?>
